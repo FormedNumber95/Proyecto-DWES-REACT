5dd8744e1742e91563c784060b0a3ff0f91ae85b
FormedNumber95 <152279572+FormedNumber95@users.noreply.github.com>
2026-01-29 12:05:04 +0100

    añadidos archivos base para frontend y backend

---
+++ grupo1/src/main/java/es/atenea/grupo1/Grupo1Application.java
@@ -0,0 +1,13 @@
+package es.atenea.grupo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Grupo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Grupo1Application.class, args);
+	}
+
+}
