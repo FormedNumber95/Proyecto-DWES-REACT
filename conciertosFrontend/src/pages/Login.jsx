446d4f9f69a35e46ed31de8e761e6bf5fa3cc0ce
lierniSR <liernisj2@gmail.com>
2026-02-15 16:02:21 +0100

    Estilo del login

--- conciertosFrontend/src/pages/Login.jsx
+++ conciertosFrontend/src/pages/Login.jsx
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
@@ -8,6 +8,14 @@ const Login = () => {
   const [pass, setPass] = useState("");
   const [err, setErr] = useState("");
   let navigate = useNavigate()
+
+  useEffect(() => {
+    document.body.classList.add('login-body');
+    return () => {
+      document.body.classList.remove('login-body');
+    };
+  }, []);
+
 
   async function login(ev) {
     ev.preventDefault();
@@ -18,7 +26,7 @@ const Login = () => {
     try {
       const datos = await axios.post("http://localhost:8090/api/auth/login", usu)
       localStorage.setItem("usuario", datos.data.rol)
-      if(datos.data.rol == "ADMIN" || datos.data.rol == "PROMOTOR"){
+      if (datos.data.rol == "ADMIN" || datos.data.rol == "PROMOTOR") {
         navigate("/home")
       }
     } catch (error) {
@@ -28,18 +36,18 @@ const Login = () => {
   }
 
   return (
-    <div>
+    <div className='div-login'>
       <h1>Login</h1>
       <form method='post'>
         <label htmlFor="email">Email: </label>
-        <input type="email" name='email' onChange={(ev) => setEmail(ev.target.value)} />
+        <input type="email" name='email' onChange={(ev) => setEmail(ev.target.value)} placeholder='Ej. ejemplo@ejemlo.com' />
         <br />
         <label htmlFor="contrasenia" >Password: </label>
-        <input type="password" name='contrasenia' onChange={(ev) => setPass(ev.target.value)} />
+        <input type="password" name='contrasenia' onChange={(ev) => setPass(ev.target.value)} placeholder='Ej. ******' />
         <br />
         <button onClick={login}>Login</button>
       </form>
-      { err!="" && <span className='error'>*Email o contraseña no válido</span>}
+      {err != "" && <span className='error-login'>* Email o contraseña no válido</span>}
     </div>
   )
 }
