56adb80aa2db7ecdfb1ca8aeeb4a5d1465e528e1
FormedNumber95 <152279572+FormedNumber95@users.noreply.github.com>
2026-05-27 13:53:37 +0200

    actualizado sql

--- sql/bdconciertos.sql
+++ sql/bdconciertos.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 25-05-2026 a las 14:49:29
+-- Tiempo de generación: 27-05-2026 a las 13:52:43
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -43,6 +43,26 @@ INSERT INTO `actuacion` (`id`, `artista_id`, `concierto_id`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `billete`
+--
+
+CREATE TABLE `billete` (
+  `id` bigint(20) NOT NULL,
+  `fecha_compra` datetime(6) DEFAULT NULL,
+  `usuario_id` bigint(20) DEFAULT NULL,
+  `transporte_id` bigint(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `billete`
+--
+
+INSERT INTO `billete` (`id`, `fecha_compra`, `usuario_id`, `transporte_id`) VALUES
+(10, '2026-05-27 11:42:33.000000', 3, 1);
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `concierto`
 --
 
@@ -60,8 +80,8 @@ CREATE TABLE `concierto` (
 --
 
 INSERT INTO `concierto` (`id`, `estado`, `fecha`, `nombre`, `precio_base`, `recinto_id`) VALUES
-(1, 'concierto1Estado', '2026-05-31 09:55:53.000000', 'concierto1Nombre', 5, 1),
-(2, 'PAKOOOO', '2026-06-10 12:41:51.000000', 'afsadf', 6, 1);
+(1, 'PROGRAMADO', '2026-05-30 10:55:00.000000', 'concierto1Nombre', 5, 1),
+(2, 'CANCELADO', '2026-06-10 12:41:00.000000', 'afsadf', 6, 1);
 
 -- --------------------------------------------------------
 
@@ -82,8 +102,51 @@ CREATE TABLE `entrada` (
 --
 
 INSERT INTO `entrada` (`id`, `cantidad`, `fecha_compra`, `usuario_id`, `tipo_entrada_id`) VALUES
-(2, 1, '2026-05-25 10:56:05.000000', 2, 1),
-(3, 1, '2026-05-25 11:49:20.000000', 2, 1);
+(8, 1, '2026-05-26 10:25:40.000000', 3, 1),
+(9, 2, '2026-05-26 10:25:40.000000', 3, 2),
+(10, 3, '2026-05-26 10:29:17.000000', 3, 2),
+(11, 1, '2026-05-26 10:31:01.000000', 3, 1),
+(12, 1, '2026-05-26 10:31:46.000000', 3, 1),
+(13, 1, '2026-05-26 10:33:02.000000', 3, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `lineapedido`
+--
+
+CREATE TABLE `lineapedido` (
+  `id` bigint(20) NOT NULL,
+  `cantidad` bigint(20) DEFAULT NULL,
+  `pedido_id` bigint(20) DEFAULT NULL,
+  `producto_id` bigint(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `pedido`
+--
+
+CREATE TABLE `pedido` (
+  `id` bigint(20) NOT NULL,
+  `fecha` datetime(6) DEFAULT NULL,
+  `usuario_id` bigint(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `producto`
+--
+
+CREATE TABLE `producto` (
+  `id` bigint(20) NOT NULL,
+  `nombre` varchar(255) DEFAULT NULL,
+  `precio` double DEFAULT NULL,
+  `stock` bigint(20) DEFAULT NULL,
+  `concierto_id` bigint(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -105,7 +168,32 @@ CREATE TABLE `tipo_entrada` (
 
 INSERT INTO `tipo_entrada` (`id`, `cupo_maximo`, `nombre`, `precio`, `concierto_id`) VALUES
 (1, 4, 'tipoEntrada1', 7, 1),
-(2, 5, 'otra', 8, 1);
+(2, 5, 'otra', 8, 1),
+(3, 5, 'CANCELADISIMO', 8, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `transporte`
+--
+
+CREATE TABLE `transporte` (
+  `id` bigint(20) NOT NULL,
+  `hora_salida` datetime(6) DEFAULT NULL,
+  `lugar_salida` varchar(255) DEFAULT NULL,
+  `plazas` int(11) NOT NULL,
+  `precio` double DEFAULT NULL,
+  `tipo` varchar(255) DEFAULT NULL,
+  `concierto_id` bigint(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `transporte`
+--
+
+INSERT INTO `transporte` (`id`, `hora_salida`, `lugar_salida`, `plazas`, `precio`, `tipo`, `concierto_id`) VALUES
+(1, '2026-05-29 11:25:00.000000', 'LugarDeSalida1', 10, 8, 'Autobus', 1),
+(2, '2026-05-27 13:01:10.000000', 'Salida Pako', 14, 3.5, 'Tren', 1);
 
 --
 -- Índices para tablas volcadas
@@ -117,6 +205,13 @@ INSERT INTO `tipo_entrada` (`id`, `cupo_maximo`, `nombre`, `precio`, `concierto_
 ALTER TABLE `actuacion`
   ADD PRIMARY KEY (`id`),
   ADD KEY `FK1kivq4qgosrjhcw6o9sxp30f0` (`concierto_id`);
+
+--
+-- Indices de la tabla `billete`
+--
+ALTER TABLE `billete`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `FKfbfpyl4tjjuu1ue43bf0k0yka` (`transporte_id`);
 
 --
 -- Indices de la tabla `concierto`
@@ -132,11 +227,39 @@ ALTER TABLE `entrada`
   ADD KEY `FKanqpsesmkjdx303yh5pvy4gdh` (`tipo_entrada_id`);
 
 --
+-- Indices de la tabla `lineapedido`
+--
+ALTER TABLE `lineapedido`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `FKqgvv8xe5j3op547echchgu31g` (`pedido_id`),
+  ADD KEY `FK930uj76crcr58q7k7v6s26wew` (`producto_id`);
+
+--
+-- Indices de la tabla `pedido`
+--
+ALTER TABLE `pedido`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `producto`
+--
+ALTER TABLE `producto`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `FKpvga8s6hjwsruvhijjfifsvqc` (`concierto_id`);
+
+--
 -- Indices de la tabla `tipo_entrada`
 --
 ALTER TABLE `tipo_entrada`
   ADD PRIMARY KEY (`id`),
   ADD KEY `FKthxieu23jwdecak321lcyib78` (`concierto_id`);
+
+--
+-- Indices de la tabla `transporte`
+--
+ALTER TABLE `transporte`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `FKkfr001on6nwts18xnpyian2d3` (`concierto_id`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
@@ -149,6 +272,12 @@ ALTER TABLE `actuacion`
   MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
+-- AUTO_INCREMENT de la tabla `billete`
+--
+ALTER TABLE `billete`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
 -- AUTO_INCREMENT de la tabla `concierto`
 --
 ALTER TABLE `concierto`
@@ -158,13 +287,37 @@ ALTER TABLE `concierto`
 -- AUTO_INCREMENT de la tabla `entrada`
 --
 ALTER TABLE `entrada`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT de la tabla `lineapedido`
+--
+ALTER TABLE `lineapedido`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `pedido`
+--
+ALTER TABLE `pedido`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `producto`
+--
+ALTER TABLE `producto`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de la tabla `tipo_entrada`
 --
 ALTER TABLE `tipo_entrada`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `transporte`
+--
+ALTER TABLE `transporte`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- Restricciones para tablas volcadas
@@ -177,16 +330,41 @@ ALTER TABLE `actuacion`
   ADD CONSTRAINT `FK1kivq4qgosrjhcw6o9sxp30f0` FOREIGN KEY (`concierto_id`) REFERENCES `concierto` (`id`);
 
 --
+-- Filtros para la tabla `billete`
+--
+ALTER TABLE `billete`
+  ADD CONSTRAINT `FKfbfpyl4tjjuu1ue43bf0k0yka` FOREIGN KEY (`transporte_id`) REFERENCES `transporte` (`id`);
+
+--
 -- Filtros para la tabla `entrada`
 --
 ALTER TABLE `entrada`
   ADD CONSTRAINT `FKanqpsesmkjdx303yh5pvy4gdh` FOREIGN KEY (`tipo_entrada_id`) REFERENCES `tipo_entrada` (`id`);
 
 --
+-- Filtros para la tabla `lineapedido`
+--
+ALTER TABLE `lineapedido`
+  ADD CONSTRAINT `FK930uj76crcr58q7k7v6s26wew` FOREIGN KEY (`producto_id`) REFERENCES `producto` (`id`),
+  ADD CONSTRAINT `FKqgvv8xe5j3op547echchgu31g` FOREIGN KEY (`pedido_id`) REFERENCES `pedido` (`id`);
+
+--
+-- Filtros para la tabla `producto`
+--
+ALTER TABLE `producto`
+  ADD CONSTRAINT `FKpvga8s6hjwsruvhijjfifsvqc` FOREIGN KEY (`concierto_id`) REFERENCES `concierto` (`id`);
+
+--
 -- Filtros para la tabla `tipo_entrada`
 --
 ALTER TABLE `tipo_entrada`
   ADD CONSTRAINT `FKthxieu23jwdecak321lcyib78` FOREIGN KEY (`concierto_id`) REFERENCES `concierto` (`id`);
+
+--
+-- Filtros para la tabla `transporte`
+--
+ALTER TABLE `transporte`
+  ADD CONSTRAINT `FKkfr001on6nwts18xnpyian2d3` FOREIGN KEY (`concierto_id`) REFERENCES `concierto` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
